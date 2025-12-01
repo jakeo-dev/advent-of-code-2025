b1a7d5316d9c8f3f120c99db19fba0cca1fbd6e7
jakeorch <jakeorchanianatjake@gmail.com>
2025-11-30 21:28:24 -0800

    add comments for day 1 part 1

--- advent-of-code-day-1/part-1.py
+++ advent-of-code-day-1/part-1.py
@@ -12,18 +12,23 @@ actual_password = 0
 for line in inpArr:
     direction = line[0]
     amount = int(line[1:])
-    
+
+    # subtract given line's amount from value if direction is left, add if right
     match direction:
         case "L":
             value -= amount
         case "R":
             value += amount
-    
+
+    # keep adding 100 if value is below zero (range is 0-99)
     while value < 0:
         value += 100
+
+    # keep subtracting 100 if value is above 99 (range is 0-99)
     while value > 99:
         value -= 100
-    
+
+    # increment actual_password when value is zero
     if value == 0:
         actual_password += 1
 
