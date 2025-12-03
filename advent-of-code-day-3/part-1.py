9141849ad0d3d8ec6054ebb2cfa5215debb4b9f4
jakeorch <jakeorchanianatjake@gmail.com>
2025-12-02 21:24:05 -0800

    day 3 part 1

---
+++ advent-of-code-day-3/part-1.py
@@ -0,0 +1,28 @@
+import math
+
+
+file1 = open("./advent-of-code-day-3/input.txt", "r")
+
+inp = file1.read()
+inpArr = inp.split("\n")
+file1.close()
+finalArr = []
+
+
+total_output_joltage = 0
+
+
+for line in inpArr:
+    # get array of batteries' joltages
+    batteries = list(line)
+    # find highest joltage excluding the last one
+    highest_1 = max(batteries[0:-1])
+    highest_1_index = batteries.index(highest_1)
+    # find second highest joltage that succeeds first highest
+    highest_2 = max(batteries[highest_1_index+1:])
+    
+    largest_joltage = int(str(highest_1) + str(highest_2))
+    
+    total_output_joltage += largest_joltage
+
+print(total_output_joltage)
